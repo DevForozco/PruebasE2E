d8ab61dfc4bc74d2f7694fa33eacf1446d8df7dc
Fabian Orozco <f.orozcob@uniandes.edu.co>
2023-05-06 17:18:33 -0500

    E2E: New post

--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,79 +1,44 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
-/**
- * Generic steps
- */
 
-When('I wait for 5 seconds', async () => {
-
+When('I enter email {string}', async function (email) {
+  let element = await this.driver.$('#ember8');
+  return await element.setValue(email);
 });
 
-When('I wait for 2 seconds', async () => {
-
+When('I enter password {string}', async function (password) {
+  let element = await this.driver.$('#ember10');
+  return await element.setValue(password);
 });
 
-When('I wait for 7 seconds', async () => {
-
+When('I click login', async function () {
+  let element = await this.driver.$('#ember12');
+  return await element.click()
 });
 
-/*
- * Feature - Creación de post 
- */
-When('I navigate to page "http://localhost:2368/ghost/#/signin"', async () => {
-
+When('I click new post', async function () {
+  let element = await this.driver.$('.ember-view.gh-btn.gh-btn-green');
+  return await element.click()
 });
 
-When('I enter email "<USERNAME>"', async () => {
-
+When('I enter post title {string}', async function (postTitle) {
+  let element = await this.driver.$(".gh-editor-title ");
+  return await element.setValue(postTitle);
 });
 
-When('I enter password "<PASSWORD>"', async () => {
-
+When('I enter post description {string}', async function (postDescription) {
+  let element = await this.driver.$('.koenig-editor__editor.__mobiledoc-editor.__has-no-content');
+  return await element.setValue(postDescription);
 });
 
-When('I click next', async () => {
-
+When('I click publish', async function () {
+  let element = await this.driver.$('.gh-publishmenu-trigger');
+  return await element.click()
 });
 
-When('I navigate to page "http://localhost:2368/ghost/#/posts"', async () => {
-
+When('I click publish now', async function () {
+  let element = await this.driver.$('.gh-publishmenu-button');
+  return await element.click()
 });
 
-When('I click new post button', async () => {
 
-});
 
-When('I enter post title "<POST_TITLE>"', async () => {
-
-});
-
-When('I writing your post "<POST_DESCRIPTION>"', async () => {
-
-});
-
-When('I enter "<POST_DESCRIPTION>"', async () => {
-
-});
-
-When('I click publish button', async () => {
-
-});
-
-When('I click Continue btn', async () => {
-
-});
-
-When('I click publish now btn', async () => {
-
-});
-
-When('I navigate to page "http://localhost:2368/ghost/#/posts?type=published"', async () => {
-
-});
-
-/**
- * Feature - BBBB
- */
-
-/**
- * Feature - CCCC
- */
