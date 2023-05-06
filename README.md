4ff1f8f0a12a4adf7d435bb43d8835270977e6d7
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2023-05-06 08:24:56 -0500

    BASE: Estuctura base del proyecto E2E
    Ghost E2E

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PruebasE2E
-Artefactos generados de las pruebas Los escenarios de pruebas deben ser ejecutados en la app
+Artefactos de pruenas E2E para funcionalidades de Ghost
+## Escenarios de prueba
+| Functionality       | Scenario "Feature"  |
+| ------------------- | ------------------- |
+| Crear post          | Publicacion de post |
+| Editar post         | Edicion de post     |
+| Eliminar post       | Inactivar post      |
+| Listar post         | Listar posts        |
+| Agendar post        | Agendar post        |
+| Crear página        | Creacion de página  |
+| Editar página       | Edición de página   |
+| Eliminar página     | Inactivar página    |
+| Listar páginas      | Listar páginas      |
+| Crear Tag           | Crear Tag           |
+| Editar Tag          | Editar Tag          |
+| Eliminar Tag        | Eliminar Tag        |
+| Visualizar Tag      | Visualizar Tag      |
+| Agregar miembro     | Agregar miembro     |
+| Editar miembro      | Editar miembro      |
+| Eliminar miembro    | Eliminar miembro    |
+| Visualizar miembros | Visualizar miembros |
---
+++ features/web/step_definitions/step.js
@@ -0,0 +1,79 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+/**
+ * Generic steps
+ */
+
+When('I wait for 5 seconds', async () => {
+
+});
+
+When('I wait for 2 seconds', async () => {
+
+});
+
+When('I wait for 7 seconds', async () => {
+
+});
+
+/*
+ * Feature - Creación de post 
+ */
+When('I navigate to page "http://localhost:2368/ghost/#/signin"', async () => {
+
+});
+
+When('I enter email "<USERNAME>"', async () => {
+
+});
+
+When('I enter password "<PASSWORD>"', async () => {
+
+});
+
+When('I click next', async () => {
+
+});
+
+When('I navigate to page "http://localhost:2368/ghost/#/posts"', async () => {
+
+});
+
+When('I click new post button', async () => {
+
+});
+
+When('I enter post title "<POST_TITLE>"', async () => {
+
+});
+
+When('I writing your post "<POST_DESCRIPTION>"', async () => {
+
+});
+
+When('I enter "<POST_DESCRIPTION>"', async () => {
+
+});
+
+When('I click publish button', async () => {
+
+});
+
+When('I click Continue btn', async () => {
+
+});
+
+When('I click publish now btn', async () => {
+
+});
+
+When('I navigate to page "http://localhost:2368/ghost/#/posts?type=published"', async () => {
+
+});
+
+/**
+ * Feature - BBBB
+ */
+
+/**
+ * Feature - CCCC
+ */
