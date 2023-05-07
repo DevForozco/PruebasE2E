fff55628bb567c2a376d8b75ec6a836aa4fe7509
Nicolas Rey <nicolasreydh@gmail.com>
2023-05-07 16:44:18 -0500

    Delete item from navigation

--- features/kraken-features/delete_navigation_item.feature
+++ features/kraken-features/delete_navigation_item.feature
@@ -1,19 +1,16 @@
-Feature: Edit website title
+Feature: Delete item from navigation
 
 @user1 @web
-Scenario: Modificar título del website
+Scenario: Eliminar un item de la navegacion
   Given I navigate to page "http://localhost:2368/ghost/#/signin"
   And I wait for 3 seconds
   When I enter email "forozcob@uniandes.edu.co"
-  And I wait for 1 seconds
   And I enter password "Pruebas123***"
   And I wait for 2 seconds
   And I click login
   And I wait for 2 seconds
-  Given I navigate to page "http://localhost:2368/ghost/#/settings/general"
+  Given I navigate to page "http://localhost:2368/ghost/#/settings/design"
   And I wait for 2 seconds
-  When I click expand button
-  When I enter site title "New site title"
+  When I click delete navigation item button
   When I click save settings
-  And I wait for 1 seconds
-  Then I get website title updated
+  And I wait for 2 seconds
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -301,7 +301,12 @@ When("I click save settings", async function () {
   return await element.click();
 });
 
-// Add item to navigation
+Then("I get website title updated", async function () {
+  let element = await this.driver.$(".gh-nav-menu-details-blog").getText();
+  expect(element).to.contain("New site title");
+});
+
+// Change website language
 When("I click last expand button", async function () {
   let element = await this.driver.$(
     ".gh-setting-last .gh-setting-action .gh-btn"
@@ -312,4 +317,10 @@ When("I click last expand button", async function () {
 When("I enter new lenguage identifier {string}", async function (siteTitle) {
   let element = await this.driver.$(".ember-text-field.gh-input.ember-view");
   return await element.setValue(siteTitle);
+});
+
+// Delete item from navigation
+When("I click delete navigation item button", async function () {
+  let element = await this.driver.$(".gh-blognav-delete");
+  return await element.click();
 });
