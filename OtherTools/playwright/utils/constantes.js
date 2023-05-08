3038da3fa5a90b85ff79e084859892dbb2310968
Fabian Orozco <f.orozcob@uniandes.edu.co>
2023-05-07 22:49:44 -0500

    Revert

--- OtherTools/playwright/utils/constantes.js
+++
@@ -1,6 +0,0 @@
-export const Constantes = {
-    urlPost: 'http://localhost:2368/ghost/#/posts',
-    urlSignin: 'http://localhost:3002/ghost/#/signin',
-    user: 'f.orozcob@uniandes.edu.co',
-    password: 'Pruebas123***'
-}
