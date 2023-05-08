7c741c255a21ef14fc6c7cf355e380e9f39adb7e
Ricardo Vivas <j.vivast@uniandes.edu.co>
2023-05-07 20:21:33 -0500

    feature: add test  delete tag, edit staff, edit email failure staff, create internal tag

--- OtherTools/playwright/edit-staff.js
+++ OtherTools/playwright/edit-staff.js
@@ -18,7 +18,7 @@ const url = 'http://localhost:2368/ghost/#/signin';
     //Abrir la URL a probar en la página y cargar el login
     await page.goto(url);
     await new Promise(r => setTimeout(r, 7000));
-    await page.screenshot({path: './create_tag_0.png'})
+    await page.screenshot({path: './edit_staff_0.png'})
 
     // Ingresar usuario y password
     await page.fill('#ember8', 'j.vivast@uniandes.edu.co');
@@ -26,31 +26,30 @@ const url = 'http://localhost:2368/ghost/#/signin';
 
     // Clic en el botón Sing in
     await new Promise(r => setTimeout(r, 7000));
-    await page.screenshot({path: './create_tag_1.png'})
+    await page.screenshot({path: './edit_staff_1.png'})
     await page.click('#ember12')
-    await new Promise(r => setTimeout(r, 9000));
-    await page.screenshot({path: './create_tag_2.png'})
-    
-    await page.click(".gh-nav-list > li:nth-child(4)");
-    await page.screenshot({path: './create_tag_3.png'});
-    
-    await new Promise(r => setTimeout(r, 2000));
-    await page.click(".view-actions > a:nth-child(2)");
-    await page.screenshot({path: './create_tag_4.png'});
+    await new Promise(r => setTimeout(r, 5000));
+    await page.screenshot({path: './edit_staff_2.png'});
 
     await new Promise(r => setTimeout(r, 3000));
-    await page.fill('#tag-name', 'Tag de prueba');
-    await page.fill('.input-color > input:nth-child(1)', 'FF4000');
-    await page.fill('#tag-slug', 'first-tag');
-    await page.fill('#tag-description', 'This is a description for my new tag');
-    await page.screenshot({path: './create_tag_5.png'});
+    await page.click('.gh-nav-list > li:nth-child(5)');
+    await page.screenshot({path: './edit_staff_3.png'});
+
+    await new Promise(r => setTimeout(r, 3000));
+    await page.click('.apps-grid > div:nth-child(3) > a');
+    await page.screenshot({path: './edit_staff_4.png'});
+
+    await new Promise(r => setTimeout(r, 3000));
+    await page.fill('#user-name', 'Name Test');
+    await page.screenshot({path: './edit_staff_5.png'});
 
     await new Promise(r => setTimeout(r, 2000));
-    await page.click(".view-actions > button:nth-child(1)");
+    await page.click('.gh-btn.gh-btn-blue');
+    await page.screenshot({path: './edit_staff_6.png'});
 
-    await new Promise(r => setTimeout(r, 2000));
-    await page.click(".gh-nav-list > li:nth-child(4)");
-    await page.screenshot({path: './create_tag_6.png'});
+    await new Promise(r => setTimeout(r, 3000));
+    await page.click('.gh-nav-list > li:nth-child(5)');
+    await page.screenshot({path: './edit_staff_7.png'});  
     
     //Finalizar la prueba
     await browser.close();
